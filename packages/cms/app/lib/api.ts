b85a5f34a46c9be45f03c5f7e0305401aacb0926
yvishwas40 <yvishwas40@gmail.com>
2026-01-12 22:16:15 +0530

    fixed api.ts in cms

--- packages/cms/app/lib/api.ts
+++ packages/cms/app/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api',
+  baseURL: 'https://ott-application.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
